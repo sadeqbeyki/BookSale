75dc90fc2baba33834888b2274e10db1c914fca3
sadeqbeyki <mardibaki@gmail.com>
2022-09-16 23:22:01 +0430

    DiscountPermissions added

---
+++ BookSale/DiscountManagement.Configuration/Permissions/DiscountPermissions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscountManagement.Configuration.Permissions
+{
+    public class DiscountPermissions
+    {
+        //Product
+        public const int ListDiscounts = 60;
+        public const int SearchDiscount = 61;
+        public const int CreateDiscount = 62;
+        public const int EditDiscount = 63;
+    }
+}
