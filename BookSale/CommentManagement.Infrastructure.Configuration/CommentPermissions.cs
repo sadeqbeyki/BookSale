34cac5c159118b4461b2822ce649f9d6a16bce30
sadeqbeyki <mardibaki@gmail.com>
2022-09-18 22:29:47 +0430

    Comment Permissions Added

---
+++ BookSale/CommentManagement.Infrastructure.Configuration/CommentPermissions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentManagement.Infrastructure.Configuration
+{
+    public class CommentPermissions
+    {
+        public const int ListComments = 70;
+        public const int SearchComments = 71;
+
+    }
+}
