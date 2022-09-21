655e276cea5cb2f6238db8905af60f81e9d3afa1
sadeqbeyki <mardibaki@gmail.com>
2022-09-21 20:23:08 +0430

    BlogPermissions Added

---
+++ BookSale/BlogManagement.Infrastructure.Configuration/BlogPermissions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogManagement.Infrastructure.Configuration
+{
+    public class BlogPermissions
+    {
+        //Articles
+        public const int ListArticles = 10;
+        public const int SearchArticles = 11;
+        public const int CreateArticle = 12;
+        public const int EditArticle = 13;
+
+        //ArticleCategories
+        public const int ListArticleCategories = 20;
+        public const int SearchArticleCategories = 21;
+        public const int CreateArticleCategories = 22;
+        public const int EditArticleCategories = 23;
+    }
+}
