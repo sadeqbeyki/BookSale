71fab44a697697635f0dfc6b86214ddb0218d494
sadeqbeyki <mardibaki@gmail.com>
2022-09-23 23:12:18 +0330

    AccountPermission added

--- BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
+++ BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommentManagement.Infrastructure.Configuration
+namespace AccountManagement.Configuration.Permissions
 {
-    public class CommentPermissionExposer
+    public class AccountPermissions
     {
     }
 }
--- BookSale/CommentManagement.Infrastructure.Configuration/Permissions/CommentPermissions.cs
+++ BookSale/CommentManagement.Infrastructure.Configuration/Permissions/CommentPermissions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommentManagement.Infrastructure.Configuration
+namespace CommentManagement.Infrastructure.Configuration.Permissions
 {
     public class CommentPermissions
     {
