42b59f9ab76967dda7d1486d7c0a62fa78f9cf5c
sadeqbeyki <mardibaki@gmail.com>
2022-09-26 23:06:52 +0330

    role add to permissens

--- BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
+++ BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
@@ -12,5 +12,10 @@ namespace AccountManagement.Configuration.Permissions
         public const int SearchUser = 11;
         public const int CreateUser = 12;
         public const int EditUser = 13;
+
+        public const int ListRoles = 10;
+        public const int SearchRole = 11;
+        public const int CreateRole = 12;
+        public const int EditRole = 13;
     }
 }
