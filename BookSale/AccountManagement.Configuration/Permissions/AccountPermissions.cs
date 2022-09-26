22eb09c5309fd936b7a10c807ba0f6d559ca3c66
sadeqbeyki <mardibaki@gmail.com>
2022-09-26 22:53:38 +0330

    AccountPermissions edited

--- BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
+++ BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
@@ -8,5 +8,9 @@ namespace AccountManagement.Configuration.Permissions
 {
     public class AccountPermissions
     {
+        public const int ListUsers = 10;
+        public const int SearchUser = 11;
+        public const int CreateUser = 12;
+        public const int EditUser = 13;
     }
 }
