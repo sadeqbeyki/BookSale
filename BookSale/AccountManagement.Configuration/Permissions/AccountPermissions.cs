1a98e9c4887bba90b7e28647921eacc5f75e8c9d
sadeqbeyki <mardibaki@gmail.com>
2022-09-27 21:38:09 +0330

    GitUpdated Permissions

--- BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
+++ BookSale/AccountManagement.Configuration/Permissions/AccountPermissions.cs
@@ -8,14 +8,14 @@ namespace AccountManagement.Configuration.Permissions
 {
     public class AccountPermissions
     {
-        public const int ListUsers = 10;
-        public const int SearchUser = 11;
-        public const int CreateUser = 12;
-        public const int EditUser = 13;
+        public const int ListUsers = 30;
+        public const int SearchUser = 31;
+        public const int CreateUser = 32;
+        public const int EditUser = 33;
 
-        public const int ListRoles = 10;
-        public const int SearchRole = 11;
-        public const int CreateRole = 12;
-        public const int EditRole = 13;
+        public const int ListRoles = 40;
+        public const int SearchRole = 41;
+        public const int CreateRole = 42;
+        public const int EditRole = 43;
     }
 }
--- BookSale/BlogManagement.Infrastructure.Configuration/Permissions/BlogPermissions.cs
+++ BookSale/BlogManagement.Infrastructure.Configuration/Permissions/BlogPermissions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlogManagement.Infrastructure.Configuration
+namespace BlogManagement.Infrastructure.Configuration.Permissions
 {
     public class BlogPermissions
     {
