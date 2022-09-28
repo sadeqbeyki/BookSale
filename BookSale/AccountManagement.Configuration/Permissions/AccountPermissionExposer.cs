6eeb66141263057297314ed82ec871053f6c8a2e
sadeqbeyki <mardibaki@gmail.com>
2022-09-28 23:22:46 +0330

    AccountPermissionExposer Implemented

--- BookSale/AccountManagement.Configuration/Permissions/AccountPermissionExposer.cs
+++ BookSale/AccountManagement.Configuration/Permissions/AccountPermissionExposer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppFramework.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace AccountManagement.Configuration.Permissions
 {
     public class AccountPermissionExposer
     {
+        public Dictionary<string, List<PermissionDto>> Expose()
+        {
+            return new Dictionary<string, List<PermissionDto>>
+            {
+                {
+                    "User", new List<PermissionDto>
+                    {
+                        new PermissionDto(AccountPermissions.ListUsers, "ListUsers"),
+                        new PermissionDto(AccountPermissions.SearchUser, "SearchUsers"),
+                        new PermissionDto(AccountPermissions.CreateUser, "CreateUsers"),
+                        new PermissionDto(AccountPermissions.EditUser, "EditUsers"),
+                    }
+                },
+                {
+                    "UserCategory", new List<PermissionDto>
+                    {
+                        new PermissionDto(AccountPermissions.ListRoles, "ListRoles"),
+                        new PermissionDto(AccountPermissions.SearchRole, "SearchRole"),
+                        new PermissionDto(AccountPermissions.CreateRole, "CreateRole"),
+                        new PermissionDto(AccountPermissions.EditRole, "EditRole"),
+                    }
+                }
+            };
+        }
     }
 }
