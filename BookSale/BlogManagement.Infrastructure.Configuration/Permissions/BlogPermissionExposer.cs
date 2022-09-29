56b3ed87d6c0d69b1718640030debba4e82b5ebd
sadeqbeyki <mardibaki@gmail.com>
2022-09-29 23:09:22 +0330

    BlogPermissionExposer Implemented

--- BookSale/BlogManagement.Infrastructure.Configuration/Permissions/BlogPermissionExposer.cs
+++ BookSale/BlogManagement.Infrastructure.Configuration/Permissions/BlogPermissionExposer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppFramework.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,29 @@ namespace BlogManagement.Infrastructure.Configuration.Permissions
 {
     public class BlogPermissionExposer
     {
-
+        public Dictionary<string, List<PermissionDto>> Expose()
+        {
+            return new Dictionary<string, List<PermissionDto>>
+            {
+                {
+                    "Article", new List<PermissionDto>
+                    {
+                        new PermissionDto(BlogPermissions.ListArticles, "ListArticles"),
+                        new PermissionDto(BlogPermissions.SearchArticles, "SearchArticles"),
+                        new PermissionDto(BlogPermissions.CreateArticle, "CreateArticles"),
+                        new PermissionDto(BlogPermissions.EditArticle, "EditArticles"),
+                    }
+                },
+                {
+                    "ArticleCategory", new List<PermissionDto>
+                    {
+                        new PermissionDto(BlogPermissions.ListArticles, "ListArticles"),
+                        new PermissionDto(BlogPermissions.SearchArticles, "SearchArticles"),
+                        new PermissionDto(BlogPermissions.CreateArticle, "CreateArticle"),
+                        new PermissionDto(BlogPermissions.EditArticle, "EditArticle"),
+                    }
+                }
+            };
+        }
     }
 }
