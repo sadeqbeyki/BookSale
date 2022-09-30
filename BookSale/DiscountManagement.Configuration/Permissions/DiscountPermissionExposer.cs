0da42b543a5466530b4f726a221cd2af55deda29
sadeqbeyki <mardibaki@gmail.com>
2022-09-30 23:18:52 +0330

    DiscountPermissionExposer Implemented

--- BookSale/DiscountManagement.Configuration/Permissions/DiscountPermissionExposer.cs
+++ BookSale/DiscountManagement.Configuration/Permissions/DiscountPermissionExposer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppFramework.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,21 @@ namespace DiscountManagement.Configuration.Permissions
 {
     public class DiscountPermissionExposer
     {
+        public Dictionary<string, List<PermissionDto>> Expose()
+        {
+            return new Dictionary<string, List<PermissionDto>>
+            {
+                {
+                    "Discount", new List<PermissionDto>
+                    {
+                    new PermissionDto(DiscountPermissions.ListDiscounts,"ListDiscounts"),
+                    new PermissionDto(DiscountPermissions.SearchDiscount,"SearchDiscount"),
+                    new PermissionDto(DiscountPermissions.CreateDiscount,"CreateDiscount"),
+                    new PermissionDto(DiscountPermissions.EditDiscount,"EditDiscount"),
 
+                    }
+                }
+            };
+        }
     }
 }
