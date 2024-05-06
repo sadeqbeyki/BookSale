a482533f1fae202a5095ea2081b8f71682c2b3f0
mardibak <mardibaki@gmail.com>
2024-05-06 23:13:30 +0330

    update sign in page part b

---
+++ BookSale/EndPoint.WebApp/Pages/SignIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EndPoint.WebApp.Pages
+{
+    public class SignInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
