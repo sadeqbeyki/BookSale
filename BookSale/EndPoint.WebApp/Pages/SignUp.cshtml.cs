ed496732b22ff50f17af3dbb0283dae35cf81f29
sadeq.beyki <mardibaki@gmail.com>
2024-05-08 22:31:58 +0330

    add sign up page

--- BookSale/EndPoint.WebApp/Pages/SignUp.cshtml.cs
+++ BookSale/EndPoint.WebApp/Pages/SignUp.cshtml.cs
@@ -4,35 +4,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EndPoint.WebApp.Pages;
 
-public class AccountModel : PageModel
+public class SignUpModel : PageModel
 {
-    [TempData]
-    public string LoginMessage { get; set; }
     [TempData]
     public string RegisterMessage { get; set; }
     private readonly IAccountApplication _accountApplication;
 
-    public AccountModel(IAccountApplication accountApplication)
+    public SignUpModel(IAccountApplication accountApplication)
     {
         _accountApplication = accountApplication;
-    }
-
-    public void OnGet()
-    {
-    }
-    public IActionResult OnPostLogin(Login command)
-    {
-        var result = _accountApplication.Login(command);
-        if (result.IsSucceeded)
-            return RedirectToAction("Index");
-
-        LoginMessage = result.Message;
-        return RedirectToPage("/Account");
-    }
-    public IActionResult OnGetLogOut()
-    {
-        _accountApplication.LogOut();
-        return RedirectToPage("Index");
     }
 
     public IActionResult OnPostRegister(RegisterAccount command)
