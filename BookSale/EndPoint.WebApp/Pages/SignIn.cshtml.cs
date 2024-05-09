0da245a09a5f2d271b62af8971348f27c1014567
sadeq.beyki <mardibaki@gmail.com>
2024-05-09 16:27:23 +0330

    update signIn page part B

--- BookSale/EndPoint.WebApp/Pages/SignIn.cshtml.cs
+++ BookSale/EndPoint.WebApp/Pages/SignIn.cshtml.cs
@@ -1,12 +1,35 @@
+using AccountManagement.Application.Contracts.Account;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EndPoint.WebApp.Pages
+namespace EndPoint.WebApp.Pages;
+
+public class SignInModel : PageModel
 {
-    public class SignInModel : PageModel
+    [TempData]
+    public string LoginMessage { get; set; }
+    private readonly IAccountApplication _accountApplication;
+
+    public SignInModel(IAccountApplication accountApplication)
     {
-        public void OnGet()
-        {
-        }
+        _accountApplication = accountApplication;
+    }
+
+    public void OnGet()
+    {
+    }
+    public IActionResult OnPostLogin(Login command)
+    {
+        var result = _accountApplication.Login(command);
+        if (result.IsSucceeded)
+            return RedirectToAction("Index");
+
+        LoginMessage = result.Message;
+        return RedirectToPage("/Account");
+    }
+    public IActionResult OnGetLogOut()
+    {
+        _accountApplication.LogOut();
+        return RedirectToPage("Index");
     }
 }
